7d8444dbd43424fc9d450052116de199ac9c324c
João Vitor Polizelli <jvitorpzl@gmail.com>
2023-02-08 22:11:46 -0300

    Initial commit

---
+++ exercicio.php
@@ -0,0 +1,44 @@
+<?php echo "2 + 2 = "; 
+echo (2+2);
+echo "<br>";
+
+echo "22 + 57 = "; 
+echo (22+57);
+echo "<br>";
+
+echo "35 - 2 = "; 
+echo (35-2);
+echo "<br>";
+
+echo "12 + 2 = "; 
+echo (12+2);
+echo "<br>";
+
+echo "124 * 7.5 = "; 
+echo (124*7.5);
+echo "<br>";
+
+echo "100 / 31 = "; 
+echo (100/31);
+echo "<br>";
+
+echo "14 + 18 - 3 = "; 
+echo (14+18-3);
+echo "<br>";
+
+echo "124 * 2 / 5 = "; 
+echo (124*2/5);
+echo "<br>";
+
+echo "(7 + 8 + 9 + 3) / 4 = "; 
+echo (7+8+9+3/4);
+echo "<br>";
+
+echo "(4 + 7)² = "; 
+echo (4+7)**2 ;
+echo "<br>";
+
+echo "(55 / 3) + 19 = "; 
+echo (55/3)+19 ;
+echo "<br>";
+?>
